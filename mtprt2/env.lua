739c4b0a7a30acda35b36b733859ccda78b8cc2e
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2024-08-12 00:36:09 +0800

    Add improvements and fixes from over the years
    incl a brand new feature - misroute recovery (good for recovering from a
    deadlock attack...)

--- mtprt2/env.lua
+++ mtprt2/env.lua
@@ -5,26 +5,49 @@ S.dists = S.dists or {}
 
 F.stn = {
     Etc = {name = "Estercroft", stops = {"EtcW", "EtcE"}},
-    Wtv = {name = "Whitevale",  stops = {"WtvN", "WtvS"}}
+    Wtv = {name = "Whitevale",  stops = {"WtvN", "WtvS"}},
+    Amy = {name = "Armya",      stops = {"Amy"}},
+    Ckw = {name = "Clarkwich",  stops = {"CkwN", "CkwS"}},
+    Cld = {name = "Cold",       stops = {"CldN", "CldS"}},
+    IrS = {name = "Ironshore",  stops = {"IrSW", "IrSE"}},
+    WdM = {name = "Wildmouth",  stops = {"WdMN", "WdMS"}}
 }
 
 function F.waypoint(name)
-    if event.train then
+    __approach_callback_mode = 1
+    wp_last_handled_train = wp_last_handled_train or -1
+
+    if event.approach and event.id ~= wp_last_handled_train and (not event.has_entered) then
+        wp_last_handled_train = event.id
+
+        if get_line() ~= "-" then
+            if F.tkfirst(get_rc()) ~= name then
+                print(name .. ": Current leg mismatch. Got " .. F.tkfirst(get_rc()))
+                local new_route = F.route(name, get_line())
+                if not new_route then
+                    print(name .. ": No route from " .. name .. " to " .. get_line() .. " - cannot recover! Check manually!")
+                    return
+                end
+                set_rc(new_route)
+            end
+        end
+    elseif event.train then
+        wp_last_handled_train = -1 -- This is really to dedupe approach events
+
         -- Update leg time
         if S.t[event.id] then
             local t_ = S.t[event.id]
             local new_time = rwt.to_secs(rwt.diff(t_.lasttime, rwt.now()))
             if new_time > 0 and ((not F.matread(S.llen, t_.last, name)) or new_time < F.matread(S.llen, t_.last, name)) then
                 print("Time from", t_.last, "to", name, "updated from",
-                    F.matread(S.llen, t_.last, name), "to", new_time)
+                      F.matread(S.llen, t_.last, name), "to", new_time)
                 F.matwrite(S.llen, t_.last, name, new_time)
             end
         end
         S.t[event.id] = {last=name, lasttime=rwt.now()}
-        
+
         -- Pop last waypoint
         if get_line() ~= "-" then
-            if F.tkfirst(get_rc()) ~= name then print(name .. ": Current leg mismatch. Got " .. F.tkfirst(get_rc())) end
             set_rc(F.tkrest(get_rc()))
             local remainingTime = S.dists[name][F.tklast(get_rc())]
             -- Actual time, excluding all delays, is about remainingTime + 11. The 1.2 is here to anticipate for delays
@@ -42,32 +65,32 @@ function F.stnguard(px,py,pz,sname)
   reserveCount = reserveCount or 0
   --local sname = sname or ""
   local p = POS(px,py,pz)
-  
+
   --print(px, event.type, event.id, last_handled_train)
-  
-  if event.approach and event.id ~= last_handled_train then
+
+  if event.approach and event.id ~= last_handled_train and (not event.has_entered) then
       last_handled_train = event.id
-      local will_dep = (reserveCount > 0) and can_set_route(p, "enter")
+      local will_dep = (get_line()=="-" or get_line()==sname) and reserveCount > 0 and can_set_route(p, "enter")
       local will_arr = (get_line() == sname)
-      
+
       if will_dep or will_arr then atc_send("A0") end -- Station rails will re-enable ARS
-      
+
       interrupt(0, {name = "approach", will_dep = will_dep, will_arr = will_arr})
-      
+
   elseif F.int(event, "approach") then
       local M = event.msg
-      
+
       if M.will_dep then
           digiline_send("lcd", "Car is arriving")
           reserveCount = reserveCount - 1
       end
-      
+
       if M.will_dep or M.will_arr then
           set_route(p, "enter")
       else
           set_route(p, "skip")
       end
-    
+
   elseif event.train then
     last_handled_train = -1 -- This is really to dedupe approach events
   elseif F.tctl(event, "reserveCount") then
@@ -81,11 +104,11 @@ function F.stnplat(pname, side)
   __approach_callback_mode = 1
   reserveDests = reserveDests or {}
   side = side or "L"
-  
+
   if event.approach then
       if #reserveDests > 0 then
           atc_set_lzb_tsr(2)
-          
+
           -- This might overheat the controller but who cares
           -- digiline_send("lcd", string.format("This car: %s", F.stn[reserveDests[1]].name or reserveDests[1]))
       end
@@ -101,37 +124,37 @@ function F.stnplat(pname, side)
         digiline_send("tctl", {msg="reserveCount", count = #reserveDests})
         digiline_send("lcd", string.format("This car: %s", F.stn[dest].name or dest))
         F.scheduletimeout()
-        
+
     else
         --print("TrainDropoff")
         set_rc("-")
         set_line("-")
         atc_set_text_inside("")
         atc_set_text_outside("")
-        
+
         atc_send("A1")
     end
   end
-  
+
   F.stn_reserve(pname)
   F.lcd_timeout()
 end
 
 function F.stn_reserve(pname)
     stationCursor = stationCursor or nil
-    
+
     if F.tctl(event, "stationList") then
         local v
         stationCursor,v = next(F.stn, stationCursor)
         if stationCursor == nil then
           stationCursor,v = next(F.stn, stationCursor)
         end
-        
+
         digiline_send("lcd", v.name)
         F.scheduletimeout()
     elseif F.tctl(event, "reserve") then
         local dest = stationCursor or "Etc" -- Is a station code
-        
+
         -- Check for cars already going to this destination
         local i,v
         for i,v in ipairs(reserveDests) do
@@ -142,13 +165,13 @@ function F.stn_reserve(pname)
             return
           end
         end
-        
+
         if F.route(pname, dest) == "" then
             digiline_send("lcd", string.format("No routes are currently available to %s.", F.stn[dest].name or dest))
             F.scheduletimeout()
             return
         end
-        
+
         table.insert(reserveDests, dest)
         digiline_send("lcd", string.format("Reserved! | %s | %s in queue", F.stn[dest].name or dest, #reserveDests))
         digiline_send("tctl", {msg="reserveCount", count = #reserveDests})
@@ -167,7 +190,7 @@ function F.lcd_timeout()
    if event.schedule and event.msg == "display_timeout" then
       clear_on = clear_on or rwt.now()
       reserveDests = reserveDests or {}
-      
+
       if rwt.diff(rwt.now(), clear_on) > 0 then
         --print("!a Retrying to timeout display in ", rwt.diff(rwt.now(), clear_on))
         schedule_in(rwt.diff(rwt.now(), clear_on), "display_timeout")
@@ -175,7 +198,7 @@ function F.lcd_timeout()
       else
         --print("!a Successfully timed out display")
       end
-      
+
       if #reserveDests == 0 then
         digiline_send("lcd", "Press the left button to pick a destination.")
       else
@@ -194,7 +217,7 @@ function F.makeRoute()
         local function isLess(a, b)
             return (b == nil) or (a < b)
         end
-        
+
         while #queue > 0 do
             i, cur = next(queue)
             for to, dist in pairs(S.llen[cur]) do
@@ -206,7 +229,7 @@ function F.makeRoute()
             end
             table.remove(queue, i)
         end
-        
+
         return myLasts, myDists
     end
 
@@ -222,19 +245,22 @@ function F.route(from, to)
         else return r(from, a) .. " " ..  to
         end
     end
-    
+
     local min = math.huge
-    local best = ""
-    --print(from, to)
-    --print(F.stn[to].stops)
+    local best_plat = nil
+
+    if (not F.stn[to]) or (not S.dists[from]) then return nil end
+
     for _,v in ipairs(F.stn[to].stops) do
         if S.dists[from][v] and S.dists[from][v] > 0 and S.dists[from][v] < min then
             min = S.dists[from][v]
-            best = r(from, v)
+            best_plat = v
         end
     end
-    
-    return best
+
+    if best_plat == nil then return nil
+    else return r(from, best_plat)
+    end
 end
 
 -- Junctions ----------------
@@ -245,10 +271,10 @@ function F.junc(px, py, pz, paths)
     enabled = enabled or 1
     local p = POS(px,py,pz)
     local paths = paths or {}
-    
-    if event.approach and event.id ~= last_handled_train then
+
+    if event.approach and event.id ~= last_handled_train and (not event.has_entered) then
         lastVisit = lastVisit or {}
-        
+
         last_handled_train = event.id
         -- Make sure the lastVisit entries exist + find the minimum
         local min = math.huge
@@ -260,10 +286,10 @@ function F.junc(px, py, pz, paths)
                 min_v = v
             end
         end
-        
+
         if get_line() ~= "-" then
             -- Navigate to next waypoint
-            -- Current station hasn't been popped yet. cadr time!
+            -- Current wpt hasn't been popped yet. cadr time!
             interrupt(0, {name = "setRoute", route = F.tkfirst(F.tkrest(get_rc()))})
         else
             -- Go in the least recently visited direction
@@ -271,17 +297,17 @@ function F.junc(px, py, pz, paths)
         end
     elseif F.int(event, "setRouteFlow") then
         local route = event.msg.route
-        
+
         if enabled == 0 then
             interrupt(10, {name = "setRouteFlow", route = route})
             return
         end
-        
+
         if last_handled_train == -1 then
-            --print(string.format("%s,%s,%s: Preferred route cleared already", px,py,pz))
+            -- print(string.format("%s,%s,%s: Preferred route cleared already", px,py,pz))
             return
         end
-        
+
         if lastVisit[route] == nil then
             print(string.format("%s,%s,%s: Requested nonexistent least visited %s", px,py,pz, route))
             return
@@ -292,30 +318,30 @@ function F.junc(px, py, pz, paths)
               route,_ = next(lastVisit, route)
             end
             if route == event.msg.route then -- Made a complete loop -> try again
-                --print(string.format("%s,%s,%s: Route %s blocked + no alts free", px,py,pz,route))
+                -- print(string.format("%s,%s,%s: Route %s blocked + no alts free", px,py,pz,route))
                 set_route(p, route)
                 interrupt(5, {name = "setRouteFlow", route = event.msg.route})
                 return
             end
         end
-        
+
         set_route(p, route)
         lastVisit[route] = rwt.now()
         -- DEBUG
         -- if route ~= event.msg.route then print(string.format("%s,%s,%s: Rerouted from %s to %s", px,py,pz,event.msg.route,route)) end
     elseif F.int(event, "setRoute") then
         local route = event.msg.route
-        
+
         if enabled == 0 then
             interrupt(10, {name = "setRoute", route = route})
             return
         end
-        
+
         if lastVisit[route] == nil then
             print(string.format("%s,%s,%s: Requested nonexistent next waypoint %s", px,py,pz, route))
             return
         end
-        
+
         set_route(p, route)
         lastVisit[route] = rwt.now()
     elseif event.train then
@@ -370,6 +396,6 @@ end
 
 function F.tkfirst(tokens) return tokens:match("%S+") end
 
-function F.tkrest(tokens) return tokens:match("%S+ (.*)") end
+function F.tkrest(tokens) return tokens:match("%S+ (.*)") or "" end
 
 function F.tklast(tokens) return (" " .. tokens):match(".* (%S+)") end
